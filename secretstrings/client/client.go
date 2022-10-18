0462d4754023d7b00c5b1d9e08fe626c42b3c96e
Beatrice-cyber <beatricezjiang@gmail.com>
2022-10-18 15:06:37 +0100

    reversed strings

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,37 @@
 package main
+
 import (
-//	"net/rpc"
+	//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"net/rpc"
+	"os"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"bufio"
+	//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+	data, err := os.ReadFile("wordlist")
+	check(err)
+	str := string(data)
+	request := stubs.Request{Message: str}
+	response := new(stubs.Response)
+	// TODO: connect to the RPC server and send the requests
+	client.Call(stubs.PremiumReverseHandler, request, response)
+	fmt.Println("Responded:" + response.Message)
+
 }
--- secretstrings/server/server.go
+++ secretstrings/server/server.go
@@ -1,23 +1,59 @@
-
 package main
+
 import (
-//	"errors"
-//	"flag"
-//	"fmt"
-//	"net"
-	"time"
+	"flag"
 	"math/rand"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
-//	"net/rpc"
+	"net"
+	"net/rpc"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"errors"
+	//	"flag"
+	//	"fmt"
+	//	"net"
+	"time"
+	//"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	//	"net/rpc"
 )
 
 /** Super-Secret `reversing a string' method we can't allow clients to see. **/
 func ReverseString(s string, i int) string {
-    time.Sleep(time.Duration(rand.Intn(i))* time.Second)
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	time.Sleep(time.Duration(rand.Intn(i)) * time.Second)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
+type SecretStringOperations struct{}
+
+func (s *SecretStringOperations) Reverse(req stubs.Request, response *stubs.Response) (err error) {
+
+	response.Message = ReverseString(req.Message, 10)
+	return
+}
+
+// terrible delay , new fast server
+func (s *SecretStringOperations) FastReverse(req stubs.Request, response *stubs.Response) (err error) {
+	response.Message = ReverseString(req.Message, 2)
+	return
+
+}
+
+func main() {
+	//we want the server to accpt some config
+	pAddr := flag.String("port", "8030", "Port to listen on")
+	flag.Parse()
+	//part of the reverse string impl
+	rand.Seed(time.Now().UnixNano())
+	// use the net/rpc package to register service
+	rpc.Register(&SecretStringOperations{})
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+	//defer means delay the execution of a function or a statement until the nearby function returns.
+	//In simple words, defer will move the execution of the statement to the very end inside a function.
+	defer listener.Close()
+	// we want the service to start accepting the communications
+	rpc.Accept(listener)
+
+}
